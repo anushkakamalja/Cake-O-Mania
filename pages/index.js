66ca6a680423fbce7832cac62993cf66773e3ab2
Anushka Kamalja <anushkakamalja@gmail.com>
2021-08-11 20:04:52 +0530

    Set up for Tailwind
    Signed-off-by: Anushka Kamalja <anushkakamalja@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Footer from "../sections/homepage/footer"
+import Header from "../sections/homepage/header"
 
 export default function Home() {
     return(
         <div>
+            <Header/>
             Hello World
             <Footer/>    
         </div>
