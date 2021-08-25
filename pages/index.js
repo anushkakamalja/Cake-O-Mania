c983b755fbc94535c304db29dd27ab9228d6b70a
Ajith <nairajith1507@gmail.com>
2021-08-25 21:54:53 +0530

    Added shop page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Hero from '../sections/homepage/Hero';
 import Footer from '../sections/homepage/Footer';
 import Header from '../sections/homepage/Header';
 import Reviews from '../sections/homepage/Reviews';
-import CakeGrid from '../components/CakeGrid';
+
 
 export default function Home() {
     return (
@@ -10,7 +10,6 @@ export default function Home() {
             <Header />
             <Hero />
             <Reviews />
-            <CakeGrid />
             <Footer />
         </div>
     );
