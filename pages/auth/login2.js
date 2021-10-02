dae8d948259c6f2e53a3eea3efd345f8dad0cd35
Mukul Pathania <mukulpathania96@gmail.com>
2021-10-02 16:37:01 +0530

    Integrate react-hook-form with signup form
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

--- pages/auth/login2.js
+++ pages/auth/login2.js
@@ -31,7 +31,10 @@ const Login = () => {
                     <input
                         {...register('email', {
                             required: { value: true, message: 'Email is required' },
-                            maxLength: 30
+                            maxLength: {
+                                value: 30,
+                                message: 'Email should not be more than 30 characters'
+                            }
                         })}
                         className="font-body mt-2 py-2 px-4 block w-full rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
                         type="email"
--- pages/auth/signup2.js
+++ pages/auth/signup2.js
@@ -1,6 +1,21 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import { FcGoogle } from 'react-icons/fc';
 
 const SignUp = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors: formErrors }
+    } = useForm();
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
+    const submitHandler = (data) => {
+        setButtonDisabled(true);
+        console.log(data);
+        setButtonDisabled(false);
+    };
     return (
         <div className="flex min-h-screen">
             <img src="/images/login_bg.jpeg" alt="Cake" className="hidden md:block max-h-screen" />
@@ -11,47 +26,106 @@ const SignUp = () => {
                 <h3 className="text-3xl md:text-5xl text-center mt-2 font-body font-bold">
                     Sign Up
                 </h3>
-                <form className="flex flex-col items-center pt-2 md:pt-8 w-full">
+                <form
+                    onSubmit={handleSubmit(submitHandler)}
+                    className="flex flex-col items-center pt-2 md:pt-8 w-full max-w-md mx-auto">
                     <input
-                        className="font-body my-2 py-2 px-4 block w-full max-w-md rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
+                        {...register('email', {
+                            required: { value: true, message: 'Email is required' },
+                            maxLength: {
+                                value: 30,
+                                message: 'Email should not have more than 30 characters'
+                            }
+                        })}
+                        className="font-body mt-2 py-2 px-4 block w-full rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
                         type="email"
-                        name=""
-                        id=""
                         placeholder="Email"
                     />
+                    {formErrors.email && (
+                        <p className="text-red-500 self-start pt-2">{formErrors.email.message}</p>
+                    )}
                     <input
-                        className="font-body my-2 py-2 px-4 block w-full max-w-md rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
+                        {...register('firstName', {
+                            required: { value: true, message: 'First name is required' },
+                            maxLength: {
+                                value: 30,
+                                message: 'First name should not have more than 30 characters'
+                            }
+                        })}
+                        className="font-body mt-2 py-2 px-4 block w-full rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
                         type="text"
-                        name=""
-                        id=""
                         placeholder="First Name"
                     />
+                    {formErrors.firstName && (
+                        <p className="text-red-500 self-start pt-2">
+                            {formErrors.firstName.message}
+                        </p>
+                    )}
                     <input
-                        className="font-body my-2 py-2 px-4 block w-full max-w-md rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
+                        {...register('lastName', {
+                            required: { value: true, message: 'Last name is required' },
+                            maxLength: {
+                                value: 30,
+                                message: 'Last name should not have more than 30 characters'
+                            }
+                        })}
+                        className="font-body mt-2 py-2 px-4 block w-full max-w-md rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
                         type="text"
-                        name=""
-                        id=""
                         placeholder="Last Name"
                     />
+                    {formErrors.lastName && (
+                        <p className="text-red-500 self-start pt-2">
+                            {formErrors.lastName.message}
+                        </p>
+                    )}
                     <input
-                        className="font-body my-2 py-2 px-4 block w-full max-w-md rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
+                        {...register('password', {
+                            required: { value: true, message: 'Password is required' },
+                            minLength: {
+                                value: 6,
+                                message: 'Password should have atleast 6 characters'
+                            },
+                            maxLength: {
+                                value: 30,
+                                message: 'Password should not contain more than 30 characters'
+                            }
+                        })}
+                        className="font-body mt-2 py-2 px-4 block w-full max-w-md rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
                         type="password"
-                        name=""
-                        id=""
                         placeholder="Password"
                     />
+                    {formErrors.password && (
+                        <p className="text-red-500 self-start pt-2">
+                            {formErrors.password.message}
+                        </p>
+                    )}
                     <input
-                        className="font-body my-2 py-2 px-4 block w-full max-w-md rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
+                        {...register('confirmPassword', {
+                            required: {
+                                value: true,
+                                message: 'Confirm your password'
+                            },
+                            validate: {
+                                matchPassword: (value) => {
+                                    return value === watch('password') || "Passwords don't match";
+                                }
+                            }
+                        })}
+                        className="font-body mt-2 py-2 px-4 block w-full max-w-md rounded text-lg focus:ring-2 focus:border-transparent focus:ring-pink-500 outline-none border-2 border-gray-300 placeholder-gray-500"
                         type="password"
-                        name=""
-                        id=""
                         placeholder="Confirm password"
                     />
+                    {formErrors.confirmPassword && (
+                        <p className="text-red-500 self-start pt-2">
+                            {formErrors.confirmPassword.message}
+                        </p>
+                    )}
 
                     <input
                         type="submit"
                         value="Sign Up"
-                        className="font-body font-semibold tracking-wider w-full max-w-md uppercase py-2 md:py-4 px-4 md:px-6 mt-4 rounded bg-black text-white text-lg md:text-xl transition duration-500 hover:bg-gray-600 cursor-pointer"
+                        disabled={buttonDisabled || Object.keys(formErrors).length !== 0}
+                        className="font-body font-semibold disabled:bg-gray-400 tracking-wider w-full max-w-md uppercase py-2 md:py-4 px-4 md:px-6 mt-4 rounded bg-black text-white text-lg md:text-xl transition duration-500 hover:bg-gray-600 cursor-pointer"
                     />
                     <div className="flex flex-col items-center pt-6 w-full max-w-md">
                         <p className="font-body text-gray-500 hover:text-black text-sm md:text-base">
