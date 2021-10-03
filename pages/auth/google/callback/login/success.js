35ba0cb20ba291422e98b7c7acb9d49a1de887da
Mukul Pathania <mukulpathania96@gmail.com>
2021-10-03 12:36:24 +0530

    Add a custom hook to use localstorage and use it in google callback page
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

--- pages/auth/google/callback/login/success.js
+++ pages/auth/google/callback/login/success.js
@@ -1,10 +1,16 @@
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
+import React from 'react';
 import PageLoader from '../../../../../components/PageLoader';
-// import useKeyValPairFromUrlHash from '../../../../../hooks/useKeyValPairFromUrlHash';
+import useKeyValPairFromUrlHash from '../../../../../hooks/useKeyValPairFromUrlHash';
+import useLocalStorage from '../../../../../hooks/useLocalStorage';
 
 const Success = () => {
-    // const router = useRouter();
-    // const { token, message } = useKeyValPairFromUrlHash(router.asPath);
+    const router = useRouter();
+    const { token } = useKeyValPairFromUrlHash(router.asPath);
+    const [, setToken] = useLocalStorage('token', '');
+    React.useEffect(() => {
+        setToken(token);
+    }, []);
     return <PageLoader />;
 };
 
