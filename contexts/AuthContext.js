ab98467e537f99100ba5c1bc22a85d35e42be97d
Mukul Pathania <mukulpathania96@gmail.com>
2021-10-03 19:40:33 +0530

    Setup react-query, create context for authentication
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

---
+++ contexts/AuthContext.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
+import Api from '../adapters/Api';
+import { checkAuthWithServer } from '../adapters/auth';
+import { useQuery } from 'react-query';
+import PageLoader from '../components/PageLoader';
+
+const AuthContext = React.createContext(undefined);
+
+const AuthProvider = ({ children }) => {
+    const [token] = useLocalStorage('token', '');
+    const { isLoading, isError, data } = useQuery(['checkAuth', token], checkAuthWithServer);
+    React.useEffect(() => {
+        Api.defaults.headers.Authorization = `Bearer ${token}`;
+    }, [token]);
+
+    if (isLoading) return <PageLoader />;
+    if (isError) {
+        return (
+            <AuthContext.Provider value={{ user: null, isAuthenticated: false }}>
+                {children}
+            </AuthContext.Provider>
+        );
+    }
+    const { data: authData } = data;
+    const value = {
+        user: {
+            email: authData.email,
+            firstName: authData.firstName,
+            lastName: authData.lastName
+        },
+        isAuthenticated: true
+    };
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+const useAuth = () => {
+    const context = React.useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error('useAuth must be used within a AuthProvider');
+    }
+    return context;
+};
+
+export default AuthProvider;
+export { useAuth };
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/tailwind.css';
+import AuthProvider from '../contexts/AuthContext';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <Component {...pageProps} />
+            </AuthProvider>
+        </QueryClientProvider>
+    );
 }
 
 export default MyApp;
