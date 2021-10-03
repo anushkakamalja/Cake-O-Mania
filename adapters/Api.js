4b0592a24cab441128d0328315a1fc9acd2941b8
Mukul Pathania <mukulpathania96@gmail.com>
2021-10-03 13:20:15 +0530

    Install axios, create an authentication adapter
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

---
+++ adapters/Api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL | 'http://localhost:5000';
+
+const Api = axios.create({
+    baseURL: baseUrl,
+    withCredentials: true,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+});
+
+export default Api;
---
+++ adapters/auth.js
@@ -0,0 +1,17 @@
+import Api from './Api';
+
+export const checkAuthWithServer = () => {
+    return Api.get('auth/verify');
+};
+
+export const logout = () => {
+    return Api.get('auth/logout');
+};
+
+export const login = (email, password) => {
+    return Api.post('auth/login', { email, password });
+};
+
+export const signUp = (email, password, firstName, lastName) => {
+    return Api.post('auth/signup', { email, password, firstName, lastName });
+};
