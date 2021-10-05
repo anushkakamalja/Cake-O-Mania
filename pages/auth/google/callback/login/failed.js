4b4cb4e5630e23a211a772a23b34bde3f54d3a0d
Mukul Pathania <mukulpathania96@gmail.com>
2021-10-05 16:06:38 +0530

    Show reason of failure in google callback pages
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

--- pages/auth/google/callback/login/failed.js
+++ pages/auth/google/callback/login/failed.js
@@ -1,13 +1,14 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 import PageLoader from '../../../../../components/PageLoader';
-// import useKeyValPairFromUrlHash from '../../../../../hooks/useKeyValPairFromUrlHash';
+import useKeyValPairFromUrlHash from '../../../../../hooks/useKeyValPairFromUrlHash';
 import link from '../../../../../link';
 
 const Failed = () => {
     const router = useRouter();
-    // const { message } = useKeyValPairFromUrlHash(router.asPath);
+    const { message } = useKeyValPairFromUrlHash(router.asPath);
     React.useEffect(() => {
+        alert(message);
         router.push(link.home);
     }, []);
     return <PageLoader />;
