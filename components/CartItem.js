ed01036d2779bf9221bd8466bdd01c879c39a671
Rohit Patil <rohitpatil07122001@gmail.com>
2021-10-10 18:35:22 +0530

    Dont let mukul see this
    Signed-off-by: Rohit Patil <rohitpatil07122001@gmail.com>

--- components/CartItem.js
+++ components/CartItem.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { ImCross } from 'react-icons/im';
 
-function CartItem(cake) {
-    return (
-        <div>
-            <tr>
-                <td className="p-4 text-center text-base ">
-                    <div className="w-32 h-32">
-                        <img src={cake.image_url} alt="img"></img>
-                    </div>
-                </td>
-                <td className="p-4 text-center ">{cake.name}</td>
-                <td className="p-4 text-center ">₹{cake.price_per_half_kg}</td>
-                <td className="p-4 text-center ">half kg</td>
+const CartItem = ({ image_url, name, price_per_half_kg }) => {
+    console.log(name);
 
-                <td className="p-4 text-center ">₹{cake.price_per_half_kg}</td>
-                <td className="p-4 text-center text-red bg-red  ">
-                    <div className="cursor-pointer">
-                        <ImCross color="red" />
-                    </div>
-                </td>
-            </tr>
-        </div>
+    return (
+        <tr>
+            <td className="p-4 text-center text-base ">
+                <div className="w-32 h-32">
+                    <img src={image_url} alt="img"></img>
+                </div>
+            </td>
+            <td className="p-4 text-center ">{name}</td>
+            <td className="p-4 text-center ">₹{price_per_half_kg}</td>
+            <td className="p-4 text-center ">half kg</td>
+
+            <td className="p-4 text-center ">₹{price_per_half_kg}</td>
+            <td className="p-4 text-center text-red bg-red  ">
+                <div className="cursor-pointer">
+                    <ImCross color="red" />
+                </div>
+            </td>
+        </tr>
     );
-}
+};
 
 export default CartItem;
