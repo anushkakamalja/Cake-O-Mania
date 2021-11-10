b9699d59dedeb1353dade65c06c8d34bea8b89b2
Anushka Kamalja <anushkakamalja@gmail.com>
2021-11-10 18:24:31 +0530

    Added rel="noreferrer"
    Signed-off-by: Anushka Kamalja <anushkakamalja@gmail.com>

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -5,25 +5,31 @@ import { FiPhone } from 'react-icons/fi';
 const Icons = () => {
     return (
         <div className=" w-96 flex flex-row justify-between ml-20 mr-20 mb-20 mt-5">
-            <a href="https://www.instagram.com/cakeomania_by_anushka/" target="_blank">
+            <a
+                href="https://www.instagram.com/cakeomania_by_anushka/"
+                target="_blank"
+                rel="noreferrer">
                 <AiOutlineInstagram
                     className="ml-4 mr-4 transform transition duration-500 hover:scale-150"
                     size={60}
                 />
             </a>
-            <a href="https://www.facebook.com/cakeomania.by.anushka/" target="_blank">
+            <a
+                href="https://www.facebook.com/cakeomania.by.anushka/"
+                target="_blank"
+                rel="noreferrer">
                 <AiOutlineFacebook
                     className="ml-4 mr-4 transform transition duration-500 hover:scale-150 "
                     size={60}
                 />
             </a>
-            <a href="https://wa.me/9619238916" target="_blank">
+            <a href="https://wa.me/9619238916" target="_blank" rel="noreferrer">
                 <AiOutlineWhatsApp
                     className="ml-4 mr-4 transform transition duration-500 hover:scale-150 "
                     size={60}
                 />
             </a>
-            <a href="tel:919619238916" target="_blank">
+            <a href="tel:919619238916" target="_blank" rel="noreferrer">
                 <FiPhone
                     className=" ml-4 mr-4 transform transition duration-500 hover:scale-150"
                     size={55}
